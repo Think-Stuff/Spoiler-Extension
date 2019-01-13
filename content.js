dfba7fdd0d162f34545425933cf0220fb379d81c
ALPHAQ2 <40581368+ALPHAQ2@users.noreply.github.com>
2019-01-13 16:00:32 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 var nodedata = [];
 var nodeparent = [];
 var node1 = [];
-var spoiler = "";
 
 
 var markup = document.getElementsByTagName("body");
@@ -9,67 +8,58 @@ markup[0].innerHTML = "<button id=\"btn2\" align=\"center\"  style=\"padding: 15
 
 document.getElementById("btn2").addEventListener("click", function(){
 	console.log("coming in");
-	for (var i = node1.length-1; i >=0; i--) {
-		var newnode = document.createTextNode(nodedata[i]);
-		nodeparent[i].replaceChild(newnode,node1[i]);
-	}
+	var elements = document.getElementsByClassName("taa")
+	for(var key in elements)
+		key.style.color = "black";
 });
 
-function spoiler() {
-	var elements = document.getElementsByTagName('*');
 
-	for (var i = 0; i < elements.length; i++) 
-	{
 
-	    var element = elements[i];
+// function spoiler() {
+function textNodesUnder(el){
+ 		var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+ 		 while(n=walk.nextNode()) a.push(n);
+		 return a;
+		}
 
-	    for (var j = 0; j < element.childNodes.length; j++) 
-	    {
-	        var node = element.childNodes[j];
 
-	        //nodetype 3 is read-only, and means text
-	        if (node.nodeType === 3)
-	         {
-	            var text = node.nodeValue;
-	            // if(getEditDistance("text"))
-	            // {
-	            // 	var p = "";
-	            // 	var l  = text.length;
-	            // 	for (var i = 0; i < text.length; i++) {
-	            // 		p+="▒";
-	            // 	}
-	            // 	continue;
-	            var replacedText = text.replace(/Trump/gi, '▒');
-	            if (replacedText !== text) 
-	            {    
-	            	nodedata.push(text);
-	            	nodeparent.push(element);
-	            	var k = document.createTextNode(replacedText);
-	                element.replaceChild(k, node);
-	                node1.push(k);
-	            }
-	       	 }
-	    }
+function tag() {
+	var nodes = textNodesUnder(document)
+
+	for (var i = 0; i < nodes.length; i++) {
+		var text = nodes[i].nodeValue;
+		
+		var m = getEditDistance(text, txt);
+		console.log("DEBUG:: Text is: " + text);
+		console.log("DEBUG:: Edit Distance: " + m);
+		console.log("DEBUG:: Spoiler text: " + txt);
+		var div = $("<span class='taa' style='background-color:black'>"+ nodes[i].nodeValue + "</span>")
+		if(m < (Math.max(text.length, txt.length) - Math.min(text.length, txt.length) + Math.cbrt(Math.min(text.length, txt.length)))){
+			$(div).insertBefore($(nodes[i]))
+			nodes[i].nodeValue =""
+			// $(parent).contents().map(function() {
+			// 	if (this.nodeType == Node.TEXT_NODE) {
+			// 		this.nodeValue = "aaa"
+			// 	}
+			// });
+	   }
 	}
 }
+
 function getEditDistance(a, b){
 	  if(a.length == 0) return b.length; 
 	  if(b.length == 0) return a.length; 
-
 	  var matrix = [];
-
 	  // increment along the first column of each row
 	  var i;
 	  for(i = 0; i <= b.length; i++){
 	    matrix[i] = [i];
 	  }
-
 	  // increment each column in the first row
 	  var j;
 	  for(j = 0; j <= a.length; j++){
 	    matrix[0][j] = j;
 	  }
-
 	  // Fill in the rest of the matrix
 	  for(i = 1; i <= b.length; i++){
 	    for(j = 1; j <= a.length; j++){
@@ -84,37 +74,14 @@ function getEditDistance(a, b){
 	  }
 	  return matrix[b.length][a.length];
 };
-var txt;
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                console.log(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-readTextFile("file:///home/anunay/Desktop/manifest/chrome-extension-blogpost-master/plots.txt");
-// alert(txt);
-// alert(txt);
-
-spoiler();
-// function textNodesUnder(el){
-//   var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
-//   while(n=walk.nextNode()) a.push(n);
-//   return a;
-// }
-
-// var nodes = textNodesUnder(document)
-
-// for (var i = 0; i < nodes.length; i++) {
-//   nodes[i].nodeValue = "yummy"
-// }
+var txt = '';
+// const fileurl = chrome.runtime.getURL("plots.txt");
+$.ajax({ type: "GET",   
+         url: 	chrome.runtime.getURL("plots.txt"),   
+         async: false,
+         success : function(text)
+         {
+             txt = text;
+         }
+});
+tag();
